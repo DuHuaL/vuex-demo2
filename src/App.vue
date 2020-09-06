988b0598c3fab3e044856e6fd9f075e8f2a1a6f4
DuHuaL <469580396@qq.com>
2020-09-06 14:08:22 +0800

    在组件b中通过计算属性简化获取store中的数据

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <coma></coma>
+    <comb></comb>
   </div>
 </template>
 
 <script>
 import coma from '@/components/coma';
+import comb from '@/components/comb';
 export default {
   name: 'App',
   components: {
-    coma
+    coma,
+    comb
   }
 };
 </script>
---
+++ src/components/comb.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    comb
+    <br>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  // 计算属性获取store中的数据，即原始写法的简化
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
